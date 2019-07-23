42fa0a93cb6979ae6c969e7ae7cd7753f64e0884
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2019-07-23 22:06:15 +0200

    Update up to Coupon

--- Spice__2/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice__2/Areas/Admin/Controllers/MenuItemController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice__2.Data;
+using Spice__2.Models;
 using Spice__2.Models.ViewModels;
 using Spice__2.Utility;
 
@@ -16,7 +17,7 @@ namespace Spice__2.Areas.Admin.Controllers
     public class MenuItemController : Controller
     {
         private readonly ApplicationDbContext _db;
-        private readonly IHostingEnvironment _hostingEivironment;
+        private readonly IHostingEnvironment _hostingEivironment;    //--we want to save Images on the server so that we need to host it.
 
         [BindProperty]
         public MenuItemViewModel MenuItemVM { get; set; }
@@ -57,8 +58,8 @@ namespace Spice__2.Areas.Admin.Controllers
                 return View(MenuItemVM);
             }
 
-            _db.MenuItem.Add(MenuItemVM.MenuItem);
-            await _db.SaveChangesAsync();
+            _db.MenuItem.Add(MenuItemVM.MenuItem); //--- Add the MenuItems to the Database.
+            await _db.SaveChangesAsync();           //-- Sync with the DB.
 
             // work on the image saving section.
 
@@ -72,14 +73,14 @@ namespace Spice__2.Areas.Admin.Controllers
             {
                 // files has been uploaded.
                 var uploads = Path.Combine(webRootPath, "images");
-                var extention = Path.GetExtension(files[0].FileName); 
+                var extention = Path.GetExtension(files[0].FileName); //--just one files allowed that can be uploaded.
 
                 using (var filesStream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extention), FileMode.Create))
                 {
-                    files[0].CopyTo(filesStream);
+                    files[0].CopyTo(filesStream);  //--it copies the file in the server and rename it.
                 }
 
-                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extention;
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extention; 
             }
              else
             {
@@ -114,7 +115,7 @@ namespace Spice__2.Areas.Admin.Controllers
 
             return View(MenuItemVM);
         }
-        //--------------------------------------------------------------------------------//
+        //-----------------------------------------------------------------------------------------------//
         //POST -- EDIT
         [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
@@ -126,15 +127,13 @@ namespace Spice__2.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());  //-- We retreiw a category Id.
 
             if (!ModelState.IsValid)
             {
+                MenuItemVM.SubCategory = await _db.SubCategories.Where(s => s.CategoryId == MenuItemVM.MenuItem.CategoryId).ToListAsync();
                 return View(MenuItemVM);
             }
-
-            //_db.MenuItem.Add(MenuItemVM.MenuItem);
-            //await _db.SaveChangesAsync();
 
             // work on the image saving section.
 
@@ -180,6 +179,25 @@ namespace Spice__2.Areas.Admin.Controllers
 
         }
         //--------------------------------------------------------------------------------//
+        //GET - DETAILS 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MenuItemVM.MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (MenuItemVM.MenuItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(MenuItemVM);
+
+        }
+        //--------------------------------------------------------------------------------//
         // GET -- DELETE
         public async Task<IActionResult> Delete(int? id)
         {
@@ -189,35 +207,41 @@ namespace Spice__2.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var menuItem = await _db.MenuItem.FindAsync(id);
+            MenuItemVM.MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
 
-            if (menuItem == null)
+            if (MenuItemVM.MenuItem == null)
             {
                 return NotFound();
             }
 
-            return View(menuItem);
+            return View(MenuItemVM.MenuItem);
         }
         //--------------------------------------------------------------------------------//
         // POST -- DELETE
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeletePOST(int? id)
+        public async Task<IActionResult> DeletePOST(int id)
         {
-            var menuItem = await _db.MenuItem.FindAsync(id);
+            string webRootPath = _hostingEivironment.WebRootPath;
+            MenuItem menuItem = await _db.MenuItem.FindAsync(id);
 
-            if (menuItem == null)
+            if (menuItem != null)
             {
-                return View();
-            }
+                var imagePath = Path.Combine(webRootPath, menuItem.Image.TrimStart('\\'));
 
-            _db.MenuItem.Remove(menuItem);
-            await _db.SaveChangesAsync();
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+
+                _db.MenuItem.Remove(menuItem);
+                await _db.SaveChangesAsync();
+            }
 
             return RedirectToAction(nameof(Index));
         }
         //--------------------------------------------------------------------------------//
-
+      
+        
     }
-
 }
