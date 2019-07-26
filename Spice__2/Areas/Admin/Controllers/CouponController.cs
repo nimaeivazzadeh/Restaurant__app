20d0d49c216e08da4e7d62a7893cb9ce06d65cea
nimaeivazzadeh <nima.eivazzadehkaljahi@yahoo.com>
2019-07-26 16:24:42 +0200

    Update up to user registration part.

--- Spice__2/Areas/Admin/Controllers/CouponController.cs
+++ Spice__2/Areas/Admin/Controllers/CouponController.cs
@@ -53,6 +53,7 @@ namespace Spice__2.Areas.Admin.Controllers
                             p1 = ms1.ToArray(); //--> It converts our Image to streams of byte and put it into p1 variable.
                         }
                     }
+
                     coupons.Picture = p1;
                 }
 
@@ -82,5 +83,103 @@ namespace Spice__2.Areas.Admin.Controllers
             return View(coupon);
         }
         //------------------------------------------------------------//
+        //EDIT -- POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Coupon coupon)
+        {
+            if (coupon.Id == 0)
+            {
+                return NotFound();
+            }
+
+            var couponFromDb = await _db.Coupon.Where(c => c.Id == coupon.Id).FirstOrDefaultAsync();
+
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+
+                    couponFromDb.Picture = p1;
+                }
+
+                couponFromDb.MinimumAmount = coupon.MinimumAmount;
+                couponFromDb.Name = coupon.Name;
+                couponFromDb.Discount = coupon.Discount;
+                couponFromDb.CouponType = coupon.CouponType;
+                couponFromDb.IsActive = coupon.IsActive;
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(coupon);
+        }
+
+
+        //------------------------------------------------------------//
+        //GET -- Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon.FirstOrDefaultAsync();  //-->Sync with DB.
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+
+        }
+
+        //------------------------------------------------------------//
+        //GET -- DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+
+        }
+        //------------------------------------------------------------//
+        //POST -- DELETE
+        [HttpPost, ActionName("Delete") ]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePOST(int? id)
+        {
+          
+            var coupon = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+                     
+            _db.Coupon.Remove(coupon);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+       //------------------------------------------------------------//
     }
 }
--- Spice__2/Areas/Customer/Controllers/HomeController.cs
+++ Spice__2/Areas/Customer/Controllers/HomeController.cs
@@ -4,18 +4,35 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice__2.Data;
 using Spice__2.Models;
+using Spice__2.Models.ViewModels;
 
 namespace Spice__2.Controllers
 {   
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        private readonly ApplicationDbContext _db; //---------------> dependency Injection.
 
+        public HomeController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+        //-----------------------------------------------------------//
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel IndexVM = new IndexViewModel() //--> We made an Object from related ViewModels. Then we want to fetch three objects such as MenuItem, Category and Coupon.
+            {
+                MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
+                Category = await _db.Category.ToListAsync(),
+                Coupon = await _db.Coupon.Where(c => c.IsActive == true).ToListAsync()
+            };
+
+            return View(IndexVM);
+        }
+        //-----------------------------------------------------------//
         public IActionResult Privacy()
         {
             return View();
